164fa86962ebe8bd008cbbfa9315a1eebf4b9fb1
Frank <11912909@mail.sustech.edu.cn>
2021-05-21 19:10:51 +0800

    add demo io files

---
+++ led_ctrl.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/05/19 23:48:39
+// Design Name: 
+// Module Name: led_ctrl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module led_ctrl(
+
+    );
+endmodule
